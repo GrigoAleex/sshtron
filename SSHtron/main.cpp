7f56fd1f6774bc3a49ff1ed265f4c10b19332423
Grigorescu Alexandru <grigorescu.aleex@gmail.com>
2024-05-20 16:20:45 +0300

    feat: map out idea

---
+++ SSHtron/main.cpp
@@ -0,0 +1,35 @@
+
+/**
+ * Learn to publish such apps
+ * Create a SRL
+ * Anti crack software
+ * Installation menu for windows, mac and linux
+ * Installation guide
+ */
+
+#include <iostream>
+#include <fstream>
+
+static void printWelcomeDialog() {
+    std::cout << "Welcome to SSHtron!" << std::endl;
+}
+
+
+int main(int argc, const char * argv[]) {
+    printWelcomeDialog();
+//    Select Connection or Register connection and quit option;
+    bool loadConnection = true;
+    if (loadConnection) {
+//        List all registered connections; and back option
+//        Wait to slect a connection
+//        Make the connections
+    } else {
+//        Always allow to user to go back by typing 'q' inform them
+//      Ask for ip adress
+//      Ask for user
+//      Ask for nickname
+//          If the nickname is left blank make it "user@ipadress"
+//      Go to homescreen
+    }
+    return 0;
+}
