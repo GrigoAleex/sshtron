5c2410bf0d0571ca5cc7949ee0cc8710aae07043
Grigorescu Alexandru <grigorescu.aleex@gmail.com>
2024-05-22 00:57:40 +0300

    build: first installer for macos

---
+++ build/CMakeFiles/sshtron.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sshtron.dir/src/main.cpp.o"
+  "CMakeFiles/sshtron.dir/src/main.cpp.o.d"
+  "sshtron"
+  "sshtron.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sshtron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sshtron.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sshtron.
+# This may be replaced when dependencies are built.
